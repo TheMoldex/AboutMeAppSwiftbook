3aba757a7781f3b6b2021b3aa59a3cdda014553c
Denis Stashkov <themoldex@gmail.com>
2023-10-06 20:32:20 +0300

    сверстал

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Денис Сташков on 6.10.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+   
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+
+    }
+}
